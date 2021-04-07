541db01d768e415d1a6c294fcc437a6dcb2c864e
SteveShay <54379252+SteveShay@users.noreply.github.com>
2021-04-07 19:15:18 -0400

    fixing missing pull. Minor changes to the budget object. Added Goals Wrapper.

--- app/src/main/java/com/example/senior_capstone_budget_app/budget/Budget.java
+++ app/src/main/java/com/example/senior_capstone_budget_app/budget/Budget.java
@@ -4,15 +4,20 @@ import android.os.Build;
 
 import androidx.annotation.RequiresApi;
 
+import com.example.senior_capstone_budget_app.transaction.Transaction;
+
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
 
 public class Budget {
     private Month month;
     private double expectedIncome;
     private double additionalIncome;
-    private double[] limits = new double[13];
+    private double[] limits = new double[9];
     private double totalExpenses;
+    private int percentTotal;
+    private int[] categoryPercents = new int[9];
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public Budget() {
@@ -27,6 +32,20 @@ public class Budget {
         this.totalExpenses = totalExpenses;
     }
 
+    public void calculateCategoryPercent(double[] categoryTotals){
+        for (int i = 0; i < categoryPercents.length; i++){
+            categoryPercents[i] = (int)((categoryTotals[i]/limits[i]) * 100);
+        }
+    }
+
+    public void calculatePercentTotal(double total) {
+        double lim = 0;
+        for (double d:limits){
+            lim += d;
+        }
+        percentTotal = (int)((lim/total)*100);
+    }
+
     //====================================Getters====================================//
     public Month getMonth() {return month;}
     public double getExpectedIncome() {return expectedIncome;}
@@ -34,6 +53,9 @@ public class Budget {
     public double[] getLimits() {return limits;}
     public double getLimit(int index) {return limits[index];}
     public double getTotalExpenses() {return totalExpenses;}
+    public int getPercentTotal() {return percentTotal;}
+    public int[] getCategoryPercents() {return categoryPercents;}
+    public int getCategoryPercent(int index) {return categoryPercents[index];}
 
     //====================================Setters====================================//
     public void setMonth(Month month) {this.month = month;}
--- app/src/main/java/com/example/senior_capstone_budget_app/goals/Goals.java
+++ app/src/main/java/com/example/senior_capstone_budget_app/goals/Goals.java
@@ -1,4 +1,40 @@
 package com.example.senior_capstone_budget_app.goals;
 
 public class Goals {
+    private Goal[] goals;
+
+    public Goals(Goal[] goals) {
+        this.goals = goals;
+    }
+
+    public int addGoal(Goal g){
+        boolean set =false;
+        for(int i = 0; i < 3; i++){
+            if (goals[i] == null){
+                goals[i] = g;
+                set = true;
+            }
+            if (!set){
+                return -1;
+            }
+        }
+        return 1;
+    }
+
+    public void removeGoal(int index){
+        goals[index] = null;
+        for(int i = index;  i < 2; i++){
+            if(goals[i] == null && goals[i+1] != null){
+                goals[i] = goals[i+1];
+            }
+        }
+    }
+
+    //====================================Getters====================================//
+    public Goal[] getGoals() {return goals;}
+    public Goal getGoal(int index) {return goals[index];}
+
+    //====================================Setters====================================//
+    public void setGoals(Goal[] goals) {this.goals = goals;}
+    public void setGoal(Goal g, int index) {this.goals[index] = g;}
 }
